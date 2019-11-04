f60ea00772fa7470562c2503835f40b1e69c7e33
Roy Steenwijk <steenwijkhawk@gmail.com>
2019-11-05 00:21:50 +0100

    Update: : 0:21:49,93

---
+++ CustomTiles.d.ts
@@ -0,0 +1,2 @@
+import { Scene, Vector2, Mesh } from "babylonjs";
+export declare function CreateCustomTiles(size: Vector2, scene: Scene): Mesh[];
---
+++ GUIElements.d.ts
@@ -0,0 +1,3 @@
+import { AdvancedDynamicTexture, Control } from "babylonjs-gui";
+import { Mesh } from "babylonjs";
+export declare function CreateGUI(guiScene: AdvancedDynamicTexture, meshToLink: Mesh): Control[];
